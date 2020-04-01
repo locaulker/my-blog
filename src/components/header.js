4a977e59893e8a96e107625e1fa61d46be61d17d
Lawrence <Caulker>
2020-04-01 17:54:30 -0400

    completed and styled Navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,34 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = (props) => {
+  const [collapsed, setCollapsed] = useState(true)
+
+  const toggleNavbar = () => setCollapsed(!collapsed)
+
+  return (
+    <div>
+      <Navbar fixed="top" light expand="sm">
+        <NavbarBrand href="/" className="mr-auto">{props.siteTitle}</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem><NavLink href="/team/">Team</NavLink></NavItem>
+            <NavItem><NavLink href="/tags">Tags</NavLink></NavItem>
+            <NavItem><NavLink href="/about">About</NavLink></NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+  )
 }
 
 export default Header
