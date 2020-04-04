bca3e42621e9f8a47fb1ca1b0eedb78b7397e74f
Lawrence <Caulker>
2020-04-04 16:59:50 -0400

    created pages and data for for author's component

---
+++ src/data/authors.js
@@ -0,0 +1,24 @@
+const authors = [
+  {
+    name: "John Doe",
+    imageUrl: john.jpg,
+    bio: "John has been a UI Designer for over 10 years. He's a very talented artist",
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+    instagram: "https://instagram.com",
+    google: "https://google.com",
+    linkedin: "https://linkedin.com",
+  },
+  {
+    name: "Jane Doe",
+    imageUrl: jane.jpg,
+    bio: "Jane is brilliant BackEnd Developer. She specializes in Security and her favorite Stack is the MERN Stack",
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+    instagram: "https://instagram.com",
+    google: "https://google.com",
+    linkedin: "https://linkedin.com",
+  },
+]
+
+module.exports = authors
