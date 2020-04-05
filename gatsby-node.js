bd076505cd4ade6070381d4e1dd38df444c8abeb
Lawrence <Caulker>
2020-04-05 08:13:18 -0400

    created all tags page programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const { slugify } = require('./src/util/utilityFunctions')
 const path = require('path')
 const authors = require('./src/util/authors')
+const _ = require('lodash')
 
 
 exports.onCreateNode = ({ node, actions }) => {
@@ -18,7 +19,11 @@ exports.onCreateNode = ({ node, actions }) => {
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
-  const singlePostTemplate = path.resolve('src/templates/single-post.js')
+
+  const templates = {
+    singlePost: path.resolve('src/templates/single-post.js'),
+    tagsPage: path.resolve('src/templates/tags-page.js')
+  }
 
   return graphql(`
     {
@@ -41,18 +46,43 @@ exports.createPages = ({ actions, graphql }) => {
     if (res.errors) return Promise.reject(res.errors)
     const posts = res.data.allMarkdownRemark.edges
 
+    // Create single blog post pages
     posts.forEach(({ node }) => (
       createPage({
         path: node.frontmatter.path,
-        component: singlePostTemplate,
+        component: templates.singlePost,
         context: {
           // slug to template single-post.js
           slug: node.frontmatter.path,
 
           // find author imageUrl from authors and pass it to single-post template
-          imageUrl: authors.find(x => x.name === node.frontmatter.author).imageUrl
+          imageUrl: authors.find(x => x.name === node.frontmatter.author).imageUrl,
         }
       })
     ))
+
+    // Get all tags
+    let tags = []
+    _.each(posts, edge => {
+      if (_.get(edge, 'node.frontmatter.tags')) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+
+    let tagPostCounts = {}
+    tags.forEach(tag => (
+      tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+    ))
+    tags = _.uniq(tags)
+
+    // Create tag page
+    createPage({
+      path: '/tags',
+      component: templates.tagsPage,
+      context: {
+        tags,
+        tagPostCounts
+      }
+    })
   })
 }
